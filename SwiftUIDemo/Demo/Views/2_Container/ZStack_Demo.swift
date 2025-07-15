3b6b7ecb49511564f5f2a9ec6c0c2d424afd75b6
meIwata <waleilulu@gmail.com>
2025-07-15 16:01:59 +0800

    圖形測試

--- SwiftUIDemo/Demo/Views/2_Container/ZStack_Demo.swift
+++ SwiftUIDemo/Demo/Views/2_Container/ZStack_Demo.swift
@@ -4,7 +4,7 @@ struct ZStack_Demo: View {
     var body: some View {
         ZStack {
             Circle()
-                .fill(.pink)
+                .fill(.pink.opacity(0.5))
             Image(systemName: "star")
                 .foregroundStyle(.green)
         }
@@ -15,10 +15,20 @@ struct ZStack_Demo: View {
             Image(systemName: "heart")
                 .foregroundStyle(.yellow)
         }
+        
+        ZStack {
+            Image("pencake")
+                .resizable()
+                .scaledToFit()
+                .cornerRadius(15) // 添加圓角
+            Image(systemName: "dog.fill")
+                .foregroundStyle(.white)
+        }
     }
 }
 
 #Preview {
     ZStack_Demo()
-        .frame(width: 48, height: 48)
+//        .frame(width: 48)
+        .frame(width: 60, height: 60)
 }
