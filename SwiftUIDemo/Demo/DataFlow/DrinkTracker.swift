3a04f8c45d605939b407a1441648175bc686abfb
meIwata <waleilulu@gmail.com>
2025-07-28 10:10:44 +0800

    Add comments and debug print to DrinkTracker
    Added explanatory comments throughout DrinkTracker.swift to clarify property usage, naming conventions, and SwiftUI concepts. Included a debug print statement in the DrinkTracker body to assist with state change debugging.

--- SwiftUIDemo/Demo/DataFlow/DrinkTracker.swift
+++ SwiftUIDemo/Demo/DataFlow/DrinkTracker.swift
@@ -1,16 +1,21 @@
 import SwiftUI
 
-struct DrinkTracker: View {
-    @State private var waterCount: Int = 0
+struct DrinkTracker: View { // struct 預設為 internal，可以寫成internal struct
+    // stored properties
+    @State private var waterCount: Int = 0 // var 變量用小駝峯命名規則
     @State private var coffeeCount: Int = 0
     @State private var beerCount: Int = 0
 
+    
     private var totalCount: Int {
+        // 每一次都做計算
         waterCount + coffeeCount + beerCount
     }
 
-    var body: some View {
-        VStack {
+    var body: some View { // 先看body，some View 是 View的子類
+        let _ = Self._printChanges(); // 在console (CMD + shift + y ，可以打開console) 看看有沒有印出，一種除錯（debug）技巧
+        VStack { // 垂直
+            // computer properties
             Text("Total number of drinks: \(totalCount)")
             WaterTracker(count: $waterCount)
             CoffeeTracker(count: $coffeeCount)
@@ -21,19 +26,19 @@ struct DrinkTracker: View {
 }
 
 struct WaterTracker2: View {
-    @State var count: Int = 0
+    @State var count: Int = 0 // @State 代表狀態的根源
 
     var body: some View {
         HStack {
             Text("^[\(count) glass](inflect: true) of water")
-            Stepper("", value: $count)
+            Stepper("", value: $count) // Stepper就是＋－計算，Stepper也有連動的用法，會同步count的值
         }
         .padding()
     }
 }
 
 struct WaterTracker: View {
-    @Binding var count: Int
+    @Binding var count: Int // @Binding 可以把外面的值同步到裡面
 
     var body: some View {
         HStack {
