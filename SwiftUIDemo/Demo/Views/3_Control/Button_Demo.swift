f68252630dafec6c0be86ee86053d8d1973bd1ef
meIwata <waleilulu@gmail.com>
2025-07-28 11:27:24 +0800

    Enhance Button_Demo with toggle state and custom button
    Added a toggleable state to Button_Demo to switch the icon and background gradient when pressed. Introduced a new CustomButton view demonstrating a similar toggle behavior with color changes. Updated previews to include CustomButton.

--- SwiftUIDemo/Demo/Views/3_Control/Button_Demo.swift
+++ SwiftUIDemo/Demo/Views/3_Control/Button_Demo.swift
@@ -1,16 +1,46 @@
 import SwiftUI
 
 struct Button_Demo: View {
+    @State private var isChanged = false
     var body: some View {
         Button {
             // save book to favorites
+            print("Button tapped: \(isChanged)")
+            isChanged.toggle() // .toogle會自動切換布林值
+            
         } label: {
-            Label("Add to Favorites", systemImage: "star")
+            Label("Add to Favorites", systemImage: isChanged ? "heart" : "heart.fill")
                 .foregroundStyle(.black)
                 .padding()
-                .background(.yellow, in: Capsule())
+                .background(
+                    LinearGradient(
+                        colors: [.yellow, .orange, .red],
+                        startPoint: .leading,
+                        endPoint: .trailing
+                    ),
+                    in: RoundedRectangle(cornerRadius: 20))
         }
     }
+}
+
+struct CustomButton: View{
+    @State private var isPressed = false
+    var body: some View{
+        Button {
+            print("Button tapped: \(isPressed)")
+            isPressed.toggle() // .toogle會自動切換布林值
+        } label: {
+            Text("Tap me")
+                .font(.title)
+                .foregroundStyle(isPressed ? .green : .red )
+            
+        }
+
+    }
+}
+
+#Preview {
+    CustomButton()
 }
 
 #Preview {
