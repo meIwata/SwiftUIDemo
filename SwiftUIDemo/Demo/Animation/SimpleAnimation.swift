945ee79bc707463b88f2fa44074731c23663ae3c
meIwata <waleilulu@gmail.com>
2025-08-04 16:02:26 +0800

    Update animation styles and parameters in demo
    Refined animation types and durations for SimpleAnimation and ImplicitAnimation views. Adjusted opacity and frame values, added comments for clarity, and experimented with different animation approaches to improve demonstration and code readability.

--- SwiftUIDemo/Demo/Animation/SimpleAnimation.swift
+++ SwiftUIDemo/Demo/Animation/SimpleAnimation.swift
@@ -8,15 +8,25 @@ struct SimpleAnimation: View {
             .foregroundStyle(.blue)
             .frame(width: flag ? 100 : 50, height: 50)
             .onTapGesture {
-                withAnimation {
-                    flag.toggle()
+               // withAnimation{ // 有一個動畫
+               // withAnimation (.spring){
+              withAnimation (.linear(duration: 2)){
+                    flag.toggle() // 觸發flag
                 } completion: {
                     isPresented.toggle()
                 }
             }
-
+        
+        /*這像是把樓拆掉重新裝潢一間房*/
+//        if isPresented{
+//            Text("Done").opacity(0) // 隱性id
+//        } else{
+//            Text("Done").opacity(1) // 隱性id
+//        }
+        
+        /*這樣寫法是直接裝修房間，這樣寫法比較好*/
         Text("Done")
-            .opacity(isPresented ? 1 : 0)
+            .opacity(isPresented ? 1 : 0.2)
     }
 }
 
@@ -30,12 +40,13 @@ struct ImplicitAnimation: View {
     var body: some View {
         RoundedRectangle(cornerRadius: 8)
             .foregroundStyle(.blue)
-            .frame(width: flag ? 100 : 50, height: 50)
+            .frame(width: flag ? 150 : 50, height: 50)
             .opacity(flag ? 1 : 0.5)
-            // Implicit Animation
-            .animation(.default, value: flag)
+            // Implicit Animation 隱性動畫
+//            .animation(.default, value: flag)
+            .animation(.spring(duration: 2), value: flag)
             .onTapGesture {
-                flag.toggle()
+                flag.toggle() // 觸發flag，整個animation都會動起來
             }
     }
 }
@@ -44,13 +55,16 @@ struct ImplicitAnimation: View {
     ImplicitAnimation()
 }
 
-struct ScopedAnimation: View {
+struct ScopedAnimation: View { // 區域範圍的動畫
     @State private var flag = false
 
     var body: some View {
         RoundedRectangle(cornerRadius: 8)
             .foregroundStyle(.blue)
             .frame(width: flag ? 100 : 50, height: 50)
+//            .animation(.linear){
+//                $0.frame(width: flag ? 100 : 50, height: 50)
+//            }
             // Scoped Animation
             .animation(.spring(duration: 2)) {
                 $0.opacity(flag ? 1 : 0.1)
